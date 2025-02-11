cc9b38d3f3dee29732aa445f25ad89ff47c26e94
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-12 01:06:06 +0530

    useState

--- prep/src/practice.jsx
+++ prep/src/practice.jsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
+
 const Practice = () => {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const sum = numbers.reduce((pre, cur) => pre + cur, 0);
+  const [count, setCount] = useState(0);
   return (
     <>
-      {/* <h1>new Array is {numbers2} </h1> */}
-      {console.log(sum)}
+      <h1>Count is : {count} </h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
     </>
   );
 };
