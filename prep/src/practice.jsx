21695e5822ee3d9e81f883840caf5a55594475e2
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-12 00:48:51 +0530

    filter & reduce

--- prep/src/practice.jsx
+++ prep/src/practice.jsx
@@ -1,10 +1,10 @@
 const Practice = () => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const numbers2 = numbers.map((num) => num * 2);
+  const sum = numbers.reduce((pre, cur) => pre + cur, 0);
   return (
     <>
       {/* <h1>new Array is {numbers2} </h1> */}
-      {console.log(numbers2)}
+      {console.log(sum)}
     </>
   );
 };
